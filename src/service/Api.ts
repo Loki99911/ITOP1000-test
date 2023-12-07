636a41f2413516da9c13bd0d883b34fef841e8e4
Loki99911 <nikita_kresik@i.ua>
2023-12-07 14:53:47 +0200

    fix deploy

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 axios.defaults.baseURL = `https://api.apilayer.com/exchangerates_data`;
-const API_KEY = import.meta.env.VITE_API_KEY;
+// const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const getSymbols = () => {
-  return axios.get(`/symbols?apikey=${API_KEY}`);
-  // return axios.get(``);
+  // return axios.get(`/symbols?apikey=${API_KEY}`);
+  return axios.get(``);
 };
 
 export const getDate = (date: string, base: string) => {
-  return axios.get(`/${date}?apikey=${API_KEY}&base=${base}`);
-  // return axios.get(``);
+  // return axios.get(`/${date}?apikey=${API_KEY}&base=${base}`);
+  console.log(date, base);
+  
+  return axios.get(``);
 };
